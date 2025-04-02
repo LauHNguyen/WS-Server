23386aaa8dfc3b55f83052745bea6117120fea85
gus <hoangnguyen.lau@gmail.com>
2025-04-02 15:30:04 +0700

    FEAT - get workspace by filter
    * FEAT - get workspace by filter
    
    * FEAT - create note
    
    ---------
    
    Co-authored-by: LauHNguyen <hoannguyen.lau@gmail.com>
    Co-authored-by: Nguyễn Văn Phong <0h9ng03@gmail.com>

--- routes/API.php
+++ routes/API.php
@@ -34,7 +34,14 @@ return function (Router $router) {
    $router->group(['prefix' => 'workspace', 'middleware' => 'auth'], function (Router $router) {
       $router->get('/', [WorkSpaceController::class, 'getWorkSpacesByIDUser']);
       $router->post('/', [WorkSpaceController::class, 'createWorkSpace']);
+      $router->get('/{IDWorkSpace}', [WorkSpaceController::class,'getWorkSpacesByIDWorkSpace']);
       $router->delete('/{id}', [WorkSpaceController::class, 'deleteWorkSpace']);
+   });
+
+   //--------------------------------------------------NOTE--------------------------------------------------//
+
+   $router->group(['prefix' => 'note', 'middleware' => 'auth'], function (Router $router) {
+      $router->post('/',[NoteController::class,'createNotewithWidget']);
    });
 
    //--------------------------------------------------PROJECT--------------------------------------------------//
@@ -70,4 +77,3 @@ return function (Router $router) {
         $router->post('/create', [StatusController::class, 'createStatus']);
       });
 };
-
